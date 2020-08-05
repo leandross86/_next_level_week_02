c43facedb84f6cca3a6971a524a461377c625c91
Leandross86 <leandrobko@gmail.com>
2020-08-04 21:07:04 -0300

    Esilizando a landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -6,12 +6,14 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
+import './styles.css'
+
 
 function Landing() {
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
-        <div id="logo-container">
+        <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
           <h2>Sua plataforma de estudo online.</h2>
         </div>
@@ -20,14 +22,14 @@ function Landing() {
           alt="Plataforma de estudos" 
           className="hero-image"
         />
-        <div id="buttons-container">
+        <div className="buttons-container">
           <a href="/" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
           </a>
           <a href="/" className="give-classes">
             <img src={giveClassesIcon} alt="Estudar"/>
-            Estudar
+            Dar aulas
           </a>
         </div>
         <span className="total-connection">
