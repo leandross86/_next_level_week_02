ef406cabb13b437d88537e3003698bbd15849e0f
Leandross86 <leandrobko@gmail.com>
2020-08-08 18:29:20 -0300

    styles reload and add form

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import React from "react";
+import "./styles.css";
 
-import './styles.css'
+import PageHeader from "../../components/PageHeader/";
+import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
-function TeacherList() {
+export default function TeacherList() {
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Estes são os proffys disponíveis.">
+    <div className="container" id="page-teacher-list">
+      <PageHeader title="Esses são os Proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dias da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Matematica", label: "Matematica" },
+              { value: "Ingles", label: "Ingles" },
+              { value: "Historia", label: "Historia" },
+              { value: "Fisica", label: "Fisica" },
+              { value: "Portugues", label: "Portugues" },
+              { value: "Quimica", label: "Quimica" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da Semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda" },
+              { value: "2", label: "Terça" },
+              { value: "3", label: "Quarta" },
+              { value: "4", label: "Quinta" },
+              { value: "5", label: "Sexta" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
 
+          <Input name="time" label="Hora" type="time" />
         </form>
       </PageHeader>
 
@@ -30,10 +48,7 @@ function TeacherList() {
         <TeacherItem />
         <TeacherItem />
         <TeacherItem />
-        <TeacherItem />
       </main>
     </div>
-  )
+  );
 }
-
-export default TeacherList;
